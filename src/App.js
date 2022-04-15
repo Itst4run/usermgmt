db78521982b658282fcea92c51eb409f4094e49e
itst4run <tarkr7@gmail.com>
2022-04-15 18:16:30 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Topbar from "./components/topbar/Topbar";
+import { BrowserRouter,
+  Routes,
+  Route,} from "react-router-dom";
+import Userlist from "./components/pages/Userlist";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Topbar/>
+    <Routes>
+    <Route path="/users" element={<Userlist />} />
+    </Routes> 
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./Topbar.css";
+export default function Topbar() {
+  return (
+    <div className="topbar">
+     <div className="topbarWrapper">
+<div className="topLeft">
+    <span className="logo">User Management</span>
+</div>
+<div className="topRight">
+<div className="topbarIconconataner">
+<img src="https://img.icons8.com/material-outlined/36/000000/appointment-reminders--v1.png"/>
+</div>
+
+</div>
+     </div>
+    </div>  
+    
+  )
+}
